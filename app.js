d5f42c64242254f556550afe31003b5535ceabd5
GABRIEL SANTOS COUTINHO <gabriel.scoutinho4@senacsp.edu.br>
2025-02-11 22:11:39 -0300

    Atualização

--- app.js
+++ app.js
@@ -4,6 +4,23 @@
  * @version 2.0
  */
 
+//============================================================================
+//Registro do service Worker
+//Se o navegador de internet suportar este recurso
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => 
+    {
+        console.log("Service worker registrado!")
+    })
+
+}
+
+//============================================================================
+
+
+
 function calcular() {
     //capturar os valores das caixas de input
     let gasolina = document.getElementById('gasolina').value
